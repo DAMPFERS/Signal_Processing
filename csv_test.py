67bd17c5f8c5fcea2bdce66a63c0daa834a32769
DAMPFERS <132689257+DAMPFERS@users.noreply.github.com>
2023-05-08 20:02:25 +0700

    Add files via upload

---
+++ csv_test.py
@@ -0,0 +1,54 @@
+import csv
+def scv_write():#Запись в сsv файл для windows
+    name_1 = "Anna"
+    name_2 = "Antony"
+    data_names = ["Anna","Anna"]
+    users_data = [
+        ("user name","user address"),
+        ["user1","address1"],
+        ["user2","address2"],
+        ["user3","address3"],
+        ]
+
+    with open("C:\PROGRAMS\Python\Server\Тест\CSV\data.csv","w",encoding="cp1251", newline = "") as file:  
+        writer = csv.writer(file, delimiter = ";")
+        writer.writerows(users_data)
+        # newline = "" если строки записываются через 1
+        # encoding="cp1251" для поддержания кириллици
+        #delimiter = ";"  разделитель м/у столбцами, априоре "," но в экселе windows по другому
+
+
+    '''for user in users_data:
+        with open("C:\PROGRAMS\Python\Server\Тест\CSV\data.csv","a") as file:
+            writer = csv.writer(file)
+            writer.writerow(
+                user
+            )
+            '''
+
+def csv_read(way):
+    table = [None] * 8
+    with open(way,"r") as file:
+        reader = csv.reader(file,delimiter = ";")
+        count = 0
+        for line in reader:
+            a = 0
+            for i in line:
+                if i == "1":
+                    a += 1
+                elif i == "0":
+                    pass
+                else:
+                    print("ERROR csv Karno")
+                    return -1
+                a = a << 1
+            a = a >> 1
+            table[count] = chr(a)
+            print(len(table[count]))
+            count += 1
+        return table
+
+if __name__ == "__main__":
+    a = csv_read("C:\PROGRAMS\Python\Server\Тест\CSV\Karno_1.csv")
+    print(a)
+    
